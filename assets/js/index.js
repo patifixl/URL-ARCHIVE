ba70697a058e3a3e280ae9d5b2fd39fafd857dab
Pati <patrycja.fixl@icloud.com>
2023-10-22 21:34:01 +0200

    Column 5

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,11 +2,13 @@ const fourthChild = document.querySelector(".column:nth-child(4)");
 const fifthChild = document.querySelector(".column:nth-child(5)");
 const sixthChild = document.querySelector(".column:nth-child(6)");
 const seventhChild = document.querySelector(".column:nth-child(7)");
+const eighthChild = document.querySelector(".column:nth-child(8)");
 
 const gridItems = fifthChild.querySelectorAll(".grid-item");
 const gridItemsOne = fourthChild.querySelectorAll(".grid-item-one");
 const gridItemsThree = sixthChild.querySelectorAll(".grid-item-three");
 const gridItemsFour = seventhChild.querySelectorAll(".grid-item-four");
+const gridItemsFive = eighthChild.querySelectorAll(".grid-item-five");
 
 let isWide = false;
 
@@ -14,6 +16,7 @@ const strzalka = fifthChild.querySelector(".strzalka");
 const arrow = fourthChild.querySelector(".arrow");
 const strzaleczka = sixthChild.querySelector(".strzaleczka");
 const pfeil = seventhChild.querySelector(".pfeil");
+const pfeilchen = eighthChild.querySelector(".pfeilchen");
 
 // Function to toggle column and change the text
 function toggleColumna() {
@@ -69,14 +72,29 @@ function toggleColumnd() {
   isWide = !isWide; // Toggle the state
 }
 
+function toggleColumne() {
+  if (isWide) {
+    eighthChild.style.flex = "0 0 6vw"; // Close the column
+    gridItemsFive.forEach((item) => (item.style.display = "none")); // Hide all grid-items
+    pfeilchen.textContent = ">"; // Change text to '>'
+  } else {
+    eighthChild.style.flex = "0 0 310vw"; // Open the column
+    gridItemsFive.forEach((item) => (item.style.display = "block")); // Show all grid-items
+    pfeilchen.textContent = "<"; // Change text to '<'
+  }
+  isWide = !isWide; // Toggle the state
+}
+
 // Hide grid-items by default (on page load)
 gridItems.forEach((item) => (item.style.display = "none"));
 gridItemsOne.forEach((item) => (item.style.display = "none"));
 gridItemsThree.forEach((item) => (item.style.display = "none"));
 gridItemsFour.forEach((item) => (item.style.display = "none"));
+gridItemsFive.forEach((item) => (item.style.display = "none"));
 
 // Add a click event listener to .strzalka
 strzalka.addEventListener("click", toggleColumn);
 arrow.addEventListener("click", toggleColumna);
 strzaleczka.addEventListener("click", toggleColumnc);
 pfeil.addEventListener("click", toggleColumnd);
+pfeilchen.addEventListener("click", toggleColumne);
